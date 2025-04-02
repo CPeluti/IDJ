debf8a206a4c43648a0dddbd85cc8d95132d91d8
Caio Peluti <cpeluti@gmail.com>
2025-04-02 12:55:06 -0300

    Added some godot ideas to the particle system (WIP)

--- include/Core/Timer.h
+++ include/Core/Timer.h
@@ -1,14 +1,30 @@
 #pragma once
+#include <string>
+#include <sstream>
 class Timer{
     public:
-        Timer(float amount);
-        void Update(float dt);
-        void Restart();
-        bool Expired();
-        float GetTime();
-        float GetAmount();
+        inline Timer(float amount):m_Amount(amount),m_Time(amount){};
+        
+        inline void Update(float dt) {m_Time += dt;};
+        inline void Restart() {m_Time = 0;};
+        inline bool Expired() {return m_Time >= m_Amount;};
 
+        inline float GetTime() { return m_Time; };
+        inline float GetAmount() { return m_Amount; };
+
+        inline void SetTime(float time) { m_Time = time; };
+        inline void SetAmount(float amount) { m_Amount = amount; };
+
+        inline std::string ToString() {
+            std::stringstream ss;
+            ss << "Target_Time: " << m_Amount << " | " << "Current_Time: " << m_Time;
+            return ss.str();
+        }
     private:
-        float amount;
-        float time;
+        float m_Amount;
+        float m_Time;
 };
+
+inline std::string format_as(Timer& e){
+    return e.ToString();
+}
