29572a8d227c11b85265387c00ca54c1d47785e1
AnaJordao <acjpjvjp@gmail.com>
2025-04-05 09:24:24 -0300

    Refactoring of the health system on the character and zombie classes

--- include/Core/HealthSystem.h
+++ include/Core/HealthSystem.h
@@ -1,22 +1,33 @@
 #include "Component.h"
 #include "Events/BasicEvent.h"
 #include "GameObject.h"
-class HealthSystem: public Component, public Observer {
-    public:
-        HealthSystem(GameObject& associated, float hp);
-        
-        void Update(float dt){}
-        void Render(){}
-        void OnEvent(Event& evt);
+#include "Lifebar.h"
+class HealthSystem : public Component, public Observer
+{
+public:
+    HealthSystem(GameObject &associated, float hp);
 
-        inline bool Is(std::string type){ return type == "HealthSystem"; }
+    void Update(float dt) {}
+    void Render() {}
+    void OnEvent(Event &evt);
 
-        float TakeDamage(float damage);
-        inline void SetHp(float value) {hp = value;}
-        float GetHp() const {return hp;}
+    inline bool Is(std::string type) { return type == "HealthSystem"; }
 
+    float TakeDamage(float damage);
+    inline void SetHp(float value) { hp = value; }
+    float GetHp() const { return hp; }
 
-        bool OnDamageTaken(OnDamageTakenEvent& evt);
-    private:
-        float hp;
+    bool OnDamageTaken(OnDamageTakenEvent &evt);
+
+    void SetLifebar(bool enable)
+    {
+        m_enableLifebar = enable;
+        m_lifebar->SetEnableLifebar(enable);
+    };
+
+private:
+    float hp;
+    float m_maxHp;
+    bool m_enableLifebar = true;
+    Lifebar *m_lifebar;
 };
