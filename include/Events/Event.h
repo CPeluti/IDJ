a7e27e65e088ddddc57ae9809ad50293617ea2de
AnaJordao <acjpjvjp@gmail.com>
2025-04-30 19:53:22 -0300

    #3 Criação do evento de interação

--- include/Events/Event.h
+++ include/Events/Event.h
@@ -4,47 +4,59 @@
 #include <string>
 #include <functional>
 
-#define BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
+#define BIND_EVENT_FN(fn) [this](auto &&...args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
 
-#define EVENT_TYPE(type)    static EventType GetStaticType() { return EventType::type; }\
-                            virtual EventType GetEventType() const override { return GetStaticType(); }\
-                            virtual const char* GetName() const override { return #type; }
+#define EVENT_TYPE(type)                                                        \
+    static EventType GetStaticType() { return EventType::type; }                \
+    virtual EventType GetEventType() const override { return GetStaticType(); } \
+    virtual const char *GetName() const override { return #type; }
 
-enum class EventType {
+enum class EventType
+{
     None = 0,
     OnCollision,
     OnDamageTaken,
-    OnDeath
+    OnDeath,
+    OnInteraction
 };
 
-class Event {
+class Event
+{
     friend class EventDispatcher;
-    public:
-        virtual EventType GetEventType() const = 0;
-        virtual const char* GetName() const = 0;
-        virtual std::string ToString() const {return GetName();}
-    protected:
-        bool m_Handled = false;
+
+public:
+    virtual EventType GetEventType() const = 0;
+    virtual const char *GetName() const = 0;
+    virtual std::string ToString() const { return GetName(); }
+
+protected:
+    bool m_Handled = false;
 };
 
-class EventDispatcher {
-    template<typename T>
-    using EventFn = std::function<bool(T&)>;
-public:
-    EventDispatcher(Event& e) : m_Event(e){}
+class EventDispatcher
+{
+    template <typename T>
+    using EventFn = std::function<bool(T &)>;
 
-    template<typename T>
-    bool Dispatch(EventFn<T> func){
-        if(m_Event.GetEventType() == T::GetStaticType()){
-            m_Event.m_Handled = func(*(T*)&m_Event);
+public:
+    EventDispatcher(Event &e) : m_Event(e) {}
+
+    template <typename T>
+    bool Dispatch(EventFn<T> func)
+    {
+        if (m_Event.GetEventType() == T::GetStaticType())
+        {
+            m_Event.m_Handled = func(*(T *)&m_Event);
             return true;
         }
         return false;
     }
+
 private:
-    Event& m_Event;
+    Event &m_Event;
 };
 
-inline std::string format_as(const Event& e){
+inline std::string format_as(const Event &e)
+{
     return e.ToString();
 }
