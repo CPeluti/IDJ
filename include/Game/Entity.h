25f1bdea9811a9ea7ade9e86ed317fe6b3eeb9b6
AnaJordao <acjpjvjp@gmail.com>
2025-04-30 17:51:39 -0300

    Implementação do sistema de efeitos de buff e debuff

--- include/Game/Entity.h
+++ include/Game/Entity.h
@@ -1,15 +1,24 @@
 #pragma once
 #include "Core/Vec2.h"
 #include "Core/Timer.h"
+#include "Effect.h"
+#include <vector>
+#include <memory>
 
 class Entity
 {
 public:
   Entity();
+  Entity(float speed);
+  inline virtual void AddEffect(std::unique_ptr<Effect> effect) { activeEffects.push_back(std::move(effect)); };
+  virtual void UpdateEffects(float dt);
+  inline virtual void SetMovementSpeed(const float &newSpeed) { m_movementSpeed = newSpeed; };
+  inline virtual float GetMovementSpeed() const { return m_movementSpeed; };
 
-private:
-  Vec2 speed;
+protected:
+  float m_movementSpeed;
   int damage;
   bool isDead;
   Timer deathTimer;
+  std::vector<std::unique_ptr<Effect>> activeEffects;
 };
--- src/Game/Entity.cpp
+++ src/Game/Entity.cpp
@@ -4,7 +4,32 @@
 
 Entity::Entity() : deathTimer(5)
 {
-  speed = Vec2(0, 0);
+  m_movementSpeed = 0;
   damage = 0;
   isDead = false;
+  activeEffects = std::vector<std::unique_ptr<Effect>>();
+}
+
+Entity::Entity(float movementSpeed) : deathTimer(5)
+{
+  this->m_movementSpeed = movementSpeed;
+  damage = 0;
+  isDead = false;
+  activeEffects = std::vector<std::unique_ptr<Effect>>();
+}
+
+void Entity::UpdateEffects(float dt)
+{
+  for (auto it = activeEffects.begin(); it != activeEffects.end();)
+  {
+    (*it)->Apply(*this, dt);
+    if ((*it)->IsExpired())
+    {
+      it = activeEffects.erase(it);
+    }
+    else
+    {
+      ++it;
+    }
+  }
 }
