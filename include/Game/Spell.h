624ccfdaceb78dde15ad490411712376910e70bd
Caio Peluti <cpeluti@gmail.com>
2025-05-27 18:15:50 -0300

    added basic spell structure

--- include/Game/Spell.h
+++ include/Game/Spell.h
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include "core/SpriteRenderer.h"
+#include "Core/ParticleSystem.h"
 
 #define SPELL_TYPE(type, element) static SpellType GetStaticType() { return SpellType::type; }\
                             virtual SpellType GetSpellType() const override { return GetStaticType(); }\
@@ -20,24 +21,40 @@ enum class SpellElement{
 };
 class Spell {
     public:
+        Spell(GameObject& associated, std::vector<std::string> modifiers, float baseDamage, SpriteRenderer baseSprite): associated(associated), modifiers(modifiers), baseDamage(baseDamage), baseSprite(baseSprite) {}
         virtual SpellType GetSpellType() const = 0;
         virtual SpellElement GetSpellElement() const = 0;
         virtual const char* GetName() const = 0;
         virtual const char* GetElement() const = 0;
         virtual std::string ToString() const {return GetName();}
-        void castSpell();
+        virtual float getDamage() = 0;
+        static void castSpell(Vec2 pos);
 
     protected:
-        std::vector<std::string> modifier;
+        GameObject& associated;
+        std::vector<std::string> modifiers;
+        float baseDamage;
+        SpriteRenderer baseSprite;
 };
 
-class FireProjectileSpell : public Spell{
+class FireProjectileSpell : public Spell, public Component, public Observer{
     public:
-        FireProjectileSpell(){
-            baseSprite
-        }
-
+        FireProjectileSpell(GameObject &associated):Spell(associated, {}, 10, {associated, "resources/img/Bullet.png", 1, 1}),Component(associated){}
         SPELL_TYPE(projectile, fire);
-    protected:
-        SpriteRenderer baseSprite;
+        inline float getDamage() {return this->baseDamage;}
+        void Update(float dt);
+        void Render();
+        void Start();
+        inline bool Is(std::string type) {return type == "FireProjectileSpell";}
+        void OnEvent(Event& evt);
+        bool targetsPlayer = false;
+    private:
+        bool OnCollision(OnCollisionEvent& evt);
+
+    private:
+        Vec2 speed;
+        float distanceLeft;
+        int damage;
+        std::weak_ptr<GameObject> particlesSystem;
+        ParticleData m_Particle;
 };
