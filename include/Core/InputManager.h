80ae6f6a512c2cf5c5d58cbc0996f22001070a45
AnaJordao <acjpjvjp@gmail.com>
2025-06-04 18:51:24 -0300

    #4 Criação do modo de escrita

--- include/Core/InputManager.h
+++ include/Core/InputManager.h
@@ -3,42 +3,45 @@
 #define INCLUDE_SDL
 #include "SDL_include.h"
 
-#define W_KEY               SDL_SCANCODE_W
-#define A_KEY               SDL_SCANCODE_A
-#define S_KEY               SDL_SCANCODE_S
-#define D_KEY               SDL_SCANCODE_D
+#define W_KEY SDL_SCANCODE_W
+#define A_KEY SDL_SCANCODE_A
+#define S_KEY SDL_SCANCODE_S
+#define D_KEY SDL_SCANCODE_D
+#define E_KEY SDL_SCANCODE_E
 
-#define LEFT_ARROW_KEY      SDL_SCANCODE_LEFT
-#define RIGHT_ARROW_KEY     SDL_SCANCODE_RIGHT
-#define UP_ARROW_KEY        SDL_SCANCODE_UP
-#define DOWN_ARROW_KEY      SDL_SCANCODE_DOWN
-#define ESCAPE_KEY          SDL_SCANCODE_ESCAPE
-#define LEFT_MOUSE_BUTTON   SDL_BUTTON_LEFT
-#define SPACE_KEY           SDL_SCANCODE_SPACE
-class InputManager{
-    public:
-        void Update ();
-        bool KeyPress (int key);
-        bool KeyRelease (int key);
-        bool IsKeyDown (int key);
-        bool MousePress (int button);
-        bool MouseRelease (int button);
-        bool IsMouseDown (int button);
-        int GetMouseX ();
-        int GetMouseY ();
-        bool QuitRequested ();
-        static InputManager& GetInstance ();
-    private:
+#define LEFT_ARROW_KEY SDL_SCANCODE_LEFT
+#define RIGHT_ARROW_KEY SDL_SCANCODE_RIGHT
+#define UP_ARROW_KEY SDL_SCANCODE_UP
+#define DOWN_ARROW_KEY SDL_SCANCODE_DOWN
+#define ESCAPE_KEY SDL_SCANCODE_ESCAPE
+#define LEFT_MOUSE_BUTTON SDL_BUTTON_LEFT
+#define SPACE_KEY SDL_SCANCODE_SPACE
+#define SHIFT_KEY SDL_SCANCODE_LSHIFT
+class InputManager
+{
+public:
+    void Update();
+    bool KeyPress(int key);
+    bool KeyRelease(int key);
+    bool IsKeyDown(int key);
+    bool MousePress(int button);
+    bool MouseRelease(int button);
+    bool IsMouseDown(int button);
+    int GetMouseX();
+    int GetMouseY();
+    bool QuitRequested();
+    static InputManager &GetInstance();
 
-        InputManager ();
-        // ~InputManager ();
+private:
+    InputManager();
+    // ~InputManager ();
 
-        bool quitRequested;
-        int updateCounter;
-        int mouseX;
-        int mouseY;
-        bool mouseState [6];
-        int mouseUpdate [6];
-        std::unordered_map<int, bool> keyState;
-        std::unordered_map<int, int> keyUpdate;
+    bool quitRequested;
+    int updateCounter;
+    int mouseX;
+    int mouseY;
+    bool mouseState[6];
+    int mouseUpdate[6];
+    std::unordered_map<int, bool> keyState;
+    std::unordered_map<int, int> keyUpdate;
 };
