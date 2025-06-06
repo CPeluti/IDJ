5a2bc784e10ee5504335543897c73d4f275961c3
Caio Peluti <cpeluti@gmail.com>
2025-06-06 12:19:59 -0300

    added functions and vector to support projectile effects

--- include/Game/Effect.h
+++ include/Game/Effect.h
@@ -1,6 +1,11 @@
 #pragma once
 #include "Spell.h"
 
+#define EFFECT_TYPE(type, element)                                              \
+  static EffectType GetStaticType() { return EffectType::type; }                \
+  virtual EffectType GetEffectType() const override { return GetStaticType(); } \
+  virtual const char *GetName() const override { return #type; }
+
 class Entity;
 
 class Effect
@@ -9,11 +14,18 @@ public:
   virtual ~Effect() = default;
   virtual void Apply(Entity &entity, float dt) = 0;
   virtual bool IsExpired() = 0;
+  virtual const char *GetName() const = 0;
+  virtual const char *GetElement() const = 0;
+  virtual std::string ToString() const { return GetName(); }
 };
 
-class SpellEffect
+class ProjectileEffect
 {
-  virtual ~SpellEffect() = default;
+public:
+  virtual ~ProjectileEffect() = default;
   virtual void Apply(Spell &spell, float dt) = 0;
   virtual bool IsExpired() = 0;
+  virtual const char *GetName() const = 0;
+  virtual const char *GetElement() const = 0;
+  virtual std::string ToString() const { return GetName(); }
 };
