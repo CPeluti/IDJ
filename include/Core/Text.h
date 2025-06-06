094507da279c92eae02b7df743381717a37e8bca
AnaJordao <acjpjvjp@gmail.com>
2025-06-06 15:32:34 -0300

    backup

--- include/Core/Text.h
+++ include/Core/Text.h
@@ -21,7 +21,8 @@ public:
         TextStyle style,
         std::string text,
         SDL_Color color,
-        int blink);
+        int blink,
+        bool cameraFollower = false);
     ~Text();
     void Update(float dt);
     void Render();
@@ -43,4 +44,5 @@ private:
     SDL_Color color;
     Timer blink;
     bool appear;
+    bool cameraFollower;
 };
