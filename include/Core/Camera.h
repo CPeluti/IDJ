34a559be65f71448ee6e9641c845b4f85234972e
AnaJordao <acjpjvjp@gmail.com>
2025-06-23 12:37:30 -0300

    #54 Adicionado tileset do castelo

--- include/Core/Camera.h
+++ include/Core/Camera.h
@@ -2,13 +2,16 @@
 
 #include "GameObject.h"
 
-class Camera {
-    public:
-        static void Follow(GameObject* newFocus);
-        static void Unfollow();
-        static void Update(float dt);
-        static Vec2 pos;
-        static Vec2 speed;
-    private:
-        static GameObject* focus;
+class Camera
+{
+public:
+    static void Follow(GameObject *newFocus);
+    static void Unfollow();
+    static void Update(float dt);
+    static Vec2 pos;
+    static Vec2 speed;
+    static float zoom;
+
+private:
+    static GameObject *focus;
 };
