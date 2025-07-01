7002f80aef320138405939be4ab43123c5a8105e
Caio Peluti <cpeluti@gmail.com>
2025-07-01 18:02:44 -0300

    partial kan-100

--- include/Game/Effect.h
+++ include/Game/Effect.h
@@ -1,22 +1,22 @@
 #pragma once
-// #include "Spell.h"
 
 #define EFFECT_TYPE(type)                                              \
   static EffectType GetStaticType() { return EffectType::type; }                \
   virtual EffectType GetEffectType() const override { return GetStaticType(); } \
   virtual const char *GetName() const override { return #type; }
 
-enum EffectType{
+enum class EffectType{
   None,
-  Slow
+  Slow,
+  Projectile,
 };
 
-template<typename T>
+template<typename T, typename type>
 class Effect
 {
 public:
   virtual ~Effect() = default;
-  virtual void Apply(T &t, float dt) = 0;
+  virtual void Apply(T &t, type value) = 0;
   virtual bool IsExpired() = 0;
   virtual EffectType GetEffectType() const = 0;
   virtual const char *GetName() const = 0;
--- src/Game/Zombie.cpp
+++ src/Game/Zombie.cpp
@@ -187,13 +187,13 @@ bool Zombie::OnInteraction(OnInteractionEvent &evt)
     {
     case InteractionType::Effect:
     {
-        std::vector<std::weak_ptr<Effect<Entity>>> effects;
-        for (auto &effect : this->m_Effects)
+        std::vector<std::weak_ptr<Effect<Entity, float>>> effects;
+        for (auto& effect : this->m_Effects)
         {
-            std::weak_ptr<Effect<Entity>> newWeakEffect = effect;
+            std::weak_ptr<Effect<Entity, float>> newWeakEffect = effect;
             effects.push_back(newWeakEffect);
         }
-        OnEffectEvent<Entity> e = OnEffectEvent<Entity>(effects);
+        OnEffectEvent<Entity, float> e = OnEffectEvent<Entity, float>(effects);
         target.subject.notify(e);
         break;
     }
