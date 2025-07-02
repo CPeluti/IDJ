e3afe46565bc3cc1379bd176b9dbdfa463209100
Caio Peluti <cpeluti@gmail.com>
2025-07-01 22:43:07 -0300

    Added the effect but the spell needs a refactor

--- src/Game/Dummy.cpp
+++ src/Game/Dummy.cpp
@@ -187,13 +187,13 @@ bool Dummy::OnInteraction(OnInteractionEvent &evt)
     {
     case InteractionType::Effect:
     {
-        std::vector<std::weak_ptr<Effect<Entity,float>>> effects;
+        std::vector<std::weak_ptr<Effect<Entity>>> effects;
         for (auto &effect : this->m_Effects)
         {
-            std::weak_ptr<Effect<Entity, float>> newWeakEffect = effect;
+            std::weak_ptr<Effect<Entity>> newWeakEffect = effect;
             effects.push_back(newWeakEffect);
         }
-        OnEffectEvent<Entity, float> e = OnEffectEvent<Entity, float>(effects);
+        OnEffectEvent<Entity> e = OnEffectEvent<Entity>(effects);
         target.subject.notify(e);
         break;
     }
