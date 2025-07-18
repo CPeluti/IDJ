bc9d410d32e77c3c3ea7376b41658460171483d6
Caio Peluti <cpeluti@gmail.com>
2025-07-18 11:53:49 -0300

    added a big tilemap

--- resources/map/dungeon-2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="dungeon-2" tilewidth="16" tileheight="16" tilecount="144" columns="12">
- <image source="Downloads/dungeon-tileset.png" width="192" height="192"/>
-</tileset>
