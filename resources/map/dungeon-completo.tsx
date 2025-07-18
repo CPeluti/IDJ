3991ff4edf76bf095c41f01bccf20555bfff066b
Caio Peluti <cpeluti@gmail.com>
2025-07-18 11:53:49 -0300

    added a big tilemap

--- resources/map/dungeon-completo.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="dungeon-completo" tilewidth="16" tileheight="16" tilecount="351" columns="27">
- <image source="Downloads/tile set completo.png" width="432" height="208"/>
-</tileset>
