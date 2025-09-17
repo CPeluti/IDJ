ce54e7aff975dd15a3f50ef9584caf224ba94946
Caio Peluti <cpeluti@gmail.com>
2025-09-17 09:24:20 -0300

    added a better edge shader and scripts to make the camera smooth when rotating the pixels, also added a better upscaler for pixel art in 3d

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,18 @@
 extends CharacterBody3D
 
-@export var speed = 14
-
+@export var speed = 19
+@export var rotation_speed = 5
 @export var fall_acceleration = 75
+@export var cameraRig: Node3D
 
 var target_velocity = Vector3.ZERO
+var target_rotation = Vector3.ZERO
+
 
 func _physics_process(delta: float) -> void:
 	var direction = Vector3.ZERO
+	var rotation = Vector3.ZERO
+	
 	if Input.is_action_pressed("move_right"):
 		direction.x+=1
 	if Input.is_action_pressed("move_left"):
@@ -16,16 +21,19 @@ func _physics_process(delta: float) -> void:
 		direction.z+=1
 	if Input.is_action_pressed("move_forward"):
 		direction.z-=1
-
+	if Input.is_action_pressed("rotate_right"):
+		rotation.x += 1
+	if Input.is_action_pressed("rotate_left"):
+		rotation.x -= 1
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		$Pivot.basis = Basis.looking_at(direction)
 
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
-
-	if not is_on_floor():
-		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+	target_velocity.z = direction.x * speed
+	target_velocity.x = direction.z * speed * -1
+	# if not is_on_floor():
+	# 	target_velocity.y = target_velocity.y - (fall_acceleration * delta)
 	
-	velocity = target_velocity
-	move_and_slide()
+	cameraRig.rotate_y(rotation.x * rotation_speed * delta)
+	var rotated_speed = target_velocity.rotated(Vector3.UP, cameraRig.rotation.y)
+	cameraRig.global_translate(rotated_speed * delta * -1)
