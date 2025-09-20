901622b1e20bf41c4177bab1f5b72b73ec11afab
Caio Peluti <cpeluti@gmail.com>
2025-09-20 15:21:18 -0300

    added lfs and smooth transition for grass

--- scripts/camera_rig.gd
+++ scripts/camera_rig.gd
@@ -9,6 +9,7 @@ var texel_error
 @onready var _snap_space := global_transform
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#RenderingServer.camera_set_scale(get_camera_rid(), Vector2(1.0, sqrt(2.0)))
 	
 	pass # Replace with function body.
 
